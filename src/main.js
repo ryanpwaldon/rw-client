15d5d482d7002c887e12fee7677e0e52e2f55e3e
Ryan Waldon <ryanpwaldon@gmail.com>
2018-12-02 17:41:48 +1100

    added fastclick

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import fastClick from 'fastclick'
 import '@/assets/css/index.scss'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 if ('scrollRestoration' in history) history.scrollRestoration = 'manual'
 
 export default new Vue({
